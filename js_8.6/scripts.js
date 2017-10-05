cf103c766a005be3c37f677d6fdf37d3b03f2f76
sqrny <32192132+sqrny@users.noreply.github.com>
2017-10-05 13:33:14 +0200

    Add files via upload

---
+++ js_8.6/scripts.js
@@ -0,0 +1,15 @@
+var a = 1;
+var b = 1;
+var value = (a * a) + (2 * a * b) - (b * b);
+//alert(value)
+//console.log('value')
+
+if ('value'>=0){console.log ('wieksze od zera ')}
+
+else {console.log ('mniejsze od zera')}
+
+
+if ('value'==0){console.log ('rowne zero')}
+
+else {console.log ('rozne od zera')}
+
